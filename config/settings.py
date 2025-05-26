9b957e98620cc125bc6584c2dc6ef4cc9b7d84cd
Alik <alik220201@gmail.com>
2025-05-26 16:55:52 +0700

    create app authors fixes

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'users',
+    'authors',
+    'books',
 ]
 
 MIDDLEWARE = [
--- users/admin.py
+++ users/admin.py
@@ -6,3 +6,4 @@ from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_filter = ("id", "email")
+    exclude = ("password",)
