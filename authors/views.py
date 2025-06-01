19c39bf4bf266815931729aaa178bd871f7c5983
Alik <alik220201@gmail.com>
2025-06-01 23:45:37 +0700

    add views for author

--- authors/views.py
+++ authors/views.py
@@ -5,20 +5,30 @@ from authors.serializers import AuthorSerializers
 
 
 class AuthorCreateApiView(generics.CreateAPIView):
+    serializer_class = AuthorSerializers
     pass
 
 
 class AuthorRetrieveApiView(generics.RetrieveAPIView):
+    serializer_class = AuthorSerializers
+    queryset = Author.objects.all()
+
     pass
 
 
 class AuthorListApiView(generics.ListAPIView):
+    serializer_class = AuthorSerializers
+    queryset = Author.objects.all()
+
     pass
 
 
 class AuthorUpdateApiView(generics.UpdateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializers
     pass
 
 
 class AuthorDestroyApiView(generics.DestroyAPIView):
+    queryset = Author.objects.all()
     pass
