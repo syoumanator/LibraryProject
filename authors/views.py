8af89ac7c529d3dbb37fdf032dca0a5f6cad9032
Alik <alik220201@gmail.com>
2025-06-02 18:06:26 +0700

    update authors

--- authors/views.py
+++ authors/views.py
@@ -4,6 +4,7 @@ from authors.models import Author
 from authors.pagination import AuthorsPagination
 from authors.serializers import AuthorSerializers
 from rest_framework.permissions import IsAdminUser, AllowAny
+from users.permissions import IsOwner
 
 
 class AuthorCreateApiView(generics.CreateAPIView):
@@ -27,7 +28,7 @@ class AuthorRetrieveApiView(generics.RetrieveAPIView):
 class AuthorUpdateApiView(generics.UpdateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializers
-    permission_classes = [IsAdminUser,]
+    permission_classes = [IsAdminUser, IsOwner]
 
 
 class AuthorDestroyApiView(generics.DestroyAPIView):
