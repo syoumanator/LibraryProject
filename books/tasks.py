af1709be23e022f469017be173b7c7de01f4e8e3
Alik <alik220201@gmail.com>
2025-06-05 05:01:13 +0700

    add task

--- books/tasks.py
+++ books/tasks.py
@@ -1,1 +1,10 @@
+from celery import shared_task
 
+from config.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+
+
+@shared_task
+def send_information(name):
+    """Уведомляет о взятии книги в аренду"""
+    send_mail(f"Вы взяли книгу {name.book}", f"Поздравляем с взятием книги, не забудьте вернуть до {name.deadline}", EMAIL_HOST_USER, [name.user.email])
