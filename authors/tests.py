620f44d8e4db1bb1499e479b249e30fb3106ee47
Alik <alik220201@gmail.com>
2025-06-04 18:05:29 +0700

    add test for book update test

--- authors/tests.py
+++ authors/tests.py
@@ -8,63 +8,62 @@ from users.models import User
 class AuthorForAdminAPITestCase(APITestCase):
 
     def setUp(self):
-        self.user = User.objects.create_superuser(username="Test", email="test@example.com", password="testpass123")
+        self.superuser = User.objects.create_superuser(username="SU", email="su@example.com", password="testpass123")
+        self.user = User.objects.create(username="User", email="user@example.com", password="testpass123")
+
         self.client.force_authenticate(user=self.user)
         self.authors = Author.objects.create(
-            first_name="Лев",
-            last_name="Толстой",
+            first_name="Александр",
+            last_name="Пушкин",
             country="Россия",
         )
 
-    def test_author_create(self):
-        """Создание Автора"""
+    def test_super_user(self):
+        """Права суперпользователя"""
+        self.client.force_authenticate(user=self.superuser)
+        update_data = {
+            "first_name": "Николай",
+            "last_name": "Гоголь",
+            "country": "Россия",
+        }
+
+        """Создание автора"""
         data = {
-            "first_name": "Александр",
-            "last_name": "Пушкин",
+            "first_name": "Лев",
+            "last_name": "Толстой",
             "country": "Россия",
         }
         response = self.client.post("/authors/create/", data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_author_detail(self):
-        """Просмотр Автора"""
-        response = self.client.get(f"/authors/{self.authors.last_name}/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_author_list(self):
-        """Просмотр Автора"""
+        """Просмотр авторов"""
         response = self.client.get(f"/authors/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_author_update(self):
-        """Обновление Автора"""
+        """Просмотр автора"""
+        response = self.client.get(f"/authors/{self.authors.last_name}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        """Обновление автора"""
+        response = self.client.put(f"/authors/{self.authors.last_name}/update/", data=update_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test__delete_superuser(self):
+        """Удаление автора"""
+        self.client.force_authenticate(user=self.superuser)
+        response = self.client.delete(f"/authors/{self.authors.last_name}/delete/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user(self):
+        """Права пользователя"""
+        self.client.force_authenticate(user=self.user)
         update_data = {
             "first_name": "Александр",
             "last_name": "Пушкин",
             "country": "Россия1",
         }
-        response = self.client.put(f"/authors/{self.authors.last_name}/update/", data=update_data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_author_delete(self):
-        """Удаление Автора"""
-        response = self.client.delete(f"/authors/{self.authors.last_name}/delete/")
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-
-class AuthorForUserAPITestCase(APITestCase):
-
-    def setUp(self):
-        self.user = User.objects.create(username="Test", email="test@example.com", password="testpass123")
-        self.client.force_authenticate(user=self.user)
-        self.authors = Author.objects.create(
-            first_name="place",
-            last_name="00:00",
-            country="act",
-        )
-
-    def test_author_create(self):
-        """Создание Автора"""
+        """Создание автора"""
         data = {
             "first_name": "Александр",
             "last_name": "Пушкин",
@@ -73,27 +72,20 @@ class AuthorForUserAPITestCase(APITestCase):
         response = self.client.post("/authors/create/", data=data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_author_detail(self):
-        """Просмотр Автора"""
-        response = self.client.get(f"/authors/{self.authors.last_name}/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_author_list(self):
-        """Просмотр Автора"""
+        """Просмотр авторов"""
         response = self.client.get(f"/authors/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_author_update(self):
-        """Обновление Автора"""
-        update_data = {
-            "first_name": "Александр",
-            "last_name": "Пушкин",
-            "country": "Россия1",
-        }
+        """Просмотр автора"""
+        response = self.client.get(f"/authors/{self.authors.last_name}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        """Обновление автора"""
         response = self.client.put(f"/authors/{self.authors.last_name}/update/", data=update_data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_author_delete(self):
-        """Удаление Автора"""
+    def test_delete_user(self):
+        """Удаление автора"""
+        self.client.force_authenticate(user=self.user)
         response = self.client.delete(f"/authors/{self.authors.last_name}/delete/")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
